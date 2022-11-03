609acf74edda660a031430a4c92acdad3d59d430
Bazelden <jack.waller@hotmail.com>
2022-11-03 14:09:31 +0000

    kata complete

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 Nightclub Array
 
 Situation -
-You're a nightclub owner and you're having trouble keeping under 18's out. Write a function that removes all of the people under the age of 18.
+You're a nightclub owner and you're having trouble keeping under 18's out. Write a function that removes all of the people under the age of 18 and return an array of patrons who are legally allowed to partayyyy. 
 
 
 a = [
@@ -34,11 +34,18 @@ const input = [
 //👉 Write the function your CodeWarriors will start with below here:
 function removeUnderage(array) {
   // code here
+  const correctAge = [];
 
   // Loop over the array
-  array.forEach((person) => {
-    console.log(person);
+  array.forEach((person, index) => {
+    if (person.age < 18) {
+      array.splice(index, 1);
+    } else {
+      correctAge.push(person.name);
+    }
   });
+  console.log(correctAge);
+  return correctAge;
 }
 
 removeUnderage(input);
