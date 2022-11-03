46fd85c43769043cab7a687eb440907fa3ed89a8
bennyfreemantle <bennyfreemantle@protonmail.com>
2022-11-03 15:36:02 +0000

    Kata completed with 4 tests

--- main.js
+++ main.js
@@ -3,49 +3,18 @@
 Nightclub Array
 
 Situation -
-You're a nightclub owner and you're having trouble keeping under 18's out. Write a function that removes all of the people under the age of 18 and return an array of patrons who are legally allowed to partayyyy. 
+You're a nightclub owner and you're having trouble keeping under 18's out. 
+Write a function that removes all of the people under the age of 18 
+and return an array of patrons who are legally allowed to partayyyy.
 
-
-a = [
-  { name: "James", age: 25 },
-  { name: "John", age: 17 },
-];
-
-b = [{ name: "James", age: 25 }];
+We must also like to mention, as token gesture we're offering free shots
+to anyone over the gae of 18, eldest first.
 
 */
 
 // Tests input
-const input = [
-  {
-    name: "Ben",
-    age: 25,
-  },
-  {
-    name: "John",
-    age: 33,
-  },
-  {
-    name: "Jack",
-    age: 17,
-  },
-];
 
 //👉 Write the function your CodeWarriors will start with below here:
-function removeUnderage(array) {
+export function removeUnderage(array) {
   // code here
-  const correctAge = [];
-
-  // Loop over the array
-  array.forEach((person, index) => {
-    if (person.age < 18) {
-      array.splice(index, 1);
-    } else {
-      correctAge.push(person.name);
-    }
-  });
-  console.log(correctAge);
-  return correctAge;
 }
-
-removeUnderage(input);
--- main.test.js
+++ main.test.js
@@ -1,1 +1,112 @@
 //👉 Write your tests below here:
+import { removeUnderage } from "./main.js";
+import { test, expect } from "@jest/globals";
+
+describe("Checking no one under the age of 18 is allowed to party", () => {
+  // We are returning an array of names
+  test("Returned results are an array of names 18 or older", () => {
+    const actual = removeUnderage([
+      {
+        name: "Ben",
+        age: 25,
+      },
+      {
+        name: "John",
+        age: 33,
+      },
+      {
+        name: "Jack",
+        age: 17,
+      },
+    ]);
+    const expected = ["Ben", "John"];
+    expect(actual).toEqual(expect.arrayContaining(expected));
+  });
+  test("Returned results are an array of names 18 or older", () => {
+    const actual = removeUnderage([
+      {
+        name: "Adam",
+        age: 7,
+      },
+      {
+        name: "Bob",
+        age: 14,
+      },
+      {
+        name: "Eve",
+        age: 44,
+      },
+      {
+        name: "Gill",
+        age: 43,
+      },
+      {
+        name: "Jack",
+        age: 90,
+      },
+      {
+        name: "Ben",
+        age: 25,
+      },
+      {
+        name: "Vincent Adult Man",
+        age: 27,
+      },
+    ]);
+    const expected = ["Eve", "Gill", "Vincent Adult Man", "Jack", "Ben"];
+    expect(actual).toEqual(expect.arrayContaining(expected));
+  });
+});
+
+describe("Checking the names are arranged by eldest first", () => {
+  // Check if the names are sorted by age
+  test("Names should be ordered by eldest first", () => {
+    const actual = removeUnderage([
+      {
+        name: "Ben",
+        age: 25,
+      },
+      {
+        name: "John",
+        age: 33,
+      },
+      {
+        name: "Jack",
+        age: 17,
+      },
+    ]);
+    const expected = ["John", "Ben"];
+    expect(actual).toEqual(expected);
+  });
+
+  test("Names should be ordered by eldest first", () => {
+    const actual = removeUnderage([
+      {
+        name: "Adam",
+        age: 7,
+      },
+      {
+        name: "Bob",
+        age: 14,
+      },
+      {
+        name: "Eve",
+        age: 44,
+      },
+      {
+        name: "Gill",
+        age: 43,
+      },
+      {
+        name: "Jack",
+        age: 90,
+      },
+      {
+        name: "Ben",
+        age: 25,
+      },
+    ]);
+    const expected = ["Jack", "Eve", "Gill", "Ben"];
+    expect(actual).toEqual(expected);
+  });
+});
